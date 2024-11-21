969df1b757ce56e0b2e403eacb1fde4a1529c87d
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2024-11-21 18:01:29 -0500

    feat: add contact form functionality
    Add supabase for form submission using serverless functions and add also
    - Limited input size at 'message' input in contact form.
    - Change output configuration of Astro to 'hybrid'.
    - Add supabase client and its configuration to connect to the supabase database.
    - Add endpoint 'contact.ts' to save form data.
    - Add serverless function to save form data.
    - Set checkOrigin to true to enable CSRF protection when sending form data in the POST method (peding security feature validation).

---
+++ src/lib/supabase.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+    import.meta.env.SUPABASE_URL,
+    import.meta.env.SUPABASE_ANON_KEY,
+);
+
+export const dbInfo = {
+    schema: import.meta.env.SUPABASE_SCHEMA,
+    formsTable: import.meta.env.SUPABASE_FORMS_TABLE,
+}
